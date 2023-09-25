a69b731d7eb2154569acf2731b49ddd18acdd80d
fabicorvelo <30451431+fabicorvelo@users.noreply.github.com>
2023-09-25 08:51:05 -0300

    Update kwitter_page.js

--- kwitter_page.js
+++ kwitter_page.js
@@ -1,5 +1,13 @@
 //LINKS DO SEU APP FIREBASE
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAcUIKebByxJu-V3ihGxqQ_KW_vogBdLCo",
   authDomain: "dsw-projet.firebaseapp.com",
@@ -9,10 +17,10 @@ const firebaseConfig = {
   appId: "1:81412015143:web:e32ac60ea40e96b1897a02",
   measurementId: "G-90ZQ6EVQVM"
 };
-    
-    
 
-    firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
     
     user_name = localStorage.getItem("user_name");
     room_name = localStorage.getItem("room_name");
