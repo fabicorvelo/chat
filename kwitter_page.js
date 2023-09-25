a90eaf435b70ec5029eed84e4ffb50830c431003
fabicorvelo <30451431+fabicorvelo@users.noreply.github.com>
2023-09-25 08:46:43 -0300

    Update kwitter_page.js

--- kwitter_page.js
+++ kwitter_page.js
@@ -1,13 +1,13 @@
 //LINKS DO SEU APP FIREBASE
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD3ZgJ4uxytSF9zbRlKH-OsL1r8QtNBF0k",
-  authDomain: "redesocial-70e77.firebaseapp.com",
-  databaseURL: "https://redesocial-70e77-default-rtdb.firebaseio.com",
-  projectId: "redesocial-70e77",
-  storageBucket: "redesocial-70e77.appspot.com",
-  messagingSenderId: "751902812611",
-  appId: "1:751902812611:web:5f73cb7fac3f5d6582b576"
+  apiKey: "AIzaSyAcUIKebByxJu-V3ihGxqQ_KW_vogBdLCo",
+  authDomain: "dsw-projet.firebaseapp.com",
+  projectId: "dsw-projet",
+  storageBucket: "dsw-projet.appspot.com",
+  messagingSenderId: "81412015143",
+  appId: "1:81412015143:web:e32ac60ea40e96b1897a02",
+  measurementId: "G-90ZQ6EVQVM"
 };
     
     
