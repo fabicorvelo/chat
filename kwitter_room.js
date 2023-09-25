61cd22d0210823db981e836fc40ec37b468f50e0
fabicorvelo <30451431+fabicorvelo@users.noreply.github.com>
2023-09-25 08:47:04 -0300

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -2,13 +2,13 @@
 //ADICIONE SEUS LINKS DO FIREBASE
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCryow5GDMc9P1BS4bmy8qY52yUR-5Z_wY",
-  authDomain: "redesocial-4501a.firebaseapp.com",
-  databaseURL: "https://redesocial-4501a-default-rtdb.firebaseio.com",
-  projectId: "redesocial-4501a",
-  storageBucket: "redesocial-4501a.appspot.com",
-  messagingSenderId: "250194306472",
-  appId: "1:250194306472:web:16a287c6092f459821e99d"
+  apiKey: "AIzaSyAcUIKebByxJu-V3ihGxqQ_KW_vogBdLCo",
+  authDomain: "dsw-projet.firebaseapp.com",
+  projectId: "dsw-projet",
+  storageBucket: "dsw-projet.appspot.com",
+  messagingSenderId: "81412015143",
+  appId: "1:81412015143:web:e32ac60ea40e96b1897a02",
+  measurementId: "G-90ZQ6EVQVM"
 };
 
 
